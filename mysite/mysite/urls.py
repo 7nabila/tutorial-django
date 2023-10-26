eec3212a5619da9014e71b77abd55aceb4df2d44
Hanifah Afkar Nabila <l200214252@student.ums.ac.id>
2023-10-26 23:50:35 +0700

    commit false

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,18 +1,10 @@
 
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import include, path
-
-urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
-=======
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('polls.urls')), #after
->>>>>>> 2463072c42a82cb8311a723d3ea446f3f14c761c
 ]
 
     #path("polls/", include("polls.urls")), #before
